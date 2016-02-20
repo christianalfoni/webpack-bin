a73ad3ce7ef7bfb59097a90147395b3673995dbf
Christian Alfoni <christianalfoni@gmail.com>
2016-02-21 00:50:15 +0100

    Added configure

---
+++ configure.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+
+var deleteFolderRecursive = function(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+deleteFolderRecursive(path.resolve('node_modules', 'babel-core'));
+
+fs.renameSync(path.resolve('node_modules', 'webpack-bin-babel-core'), path.resolve('node_modules', 'babel-core'));
